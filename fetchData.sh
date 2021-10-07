5ed30d551d7e77a7f2ee71164e9f1b29ec9b5e33
Jan van Brügge <supermanitu@gmail.com>
2021-10-07 13:21:58 +0200

    Fetch the latest dataset in CI instead of committing it

--- fetchData.sh
+++ fetchData.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# You need to setup a kubeconfig according to https://welcome.play.futurice.com/
 POD_ID=$(kubectl get pods -l=app=futulog -o json | jq -r '.items[].metadata.name')
 
-kubectl exec -it $POD_ID -- bash -c \
+kubectl exec -t $POD_ID -- bash -c \
   "psql \$DB_URL -c \"\\copy (SELECT * FROM registrations WHERE date <= CURRENT_DATE AND workmode = 'Office') TO STDOUT WITH csv\"" \
   > generator/registrations.csv
-
-gpg --output generator/registrations.csv.gpg --symmetric --cipher-algo AES256 generator/registrations.csv
