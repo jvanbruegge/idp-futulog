09c6ed7e0b6d6247bc06eb529caed47f6e9c07dc
Jan van Brügge <jan.van.brugge@futurice.com>
2021-10-12 15:32:21 +0200

    Use the slider to animate the bar chart

--- page/pairs_bars.ts
+++ page/pairs_bars.ts
@@ -1,16 +1,9 @@
-import {
-  select,
-  scaleLinear,
-  scaleSymlog,
-  max,
-  axisBottom,
-  axisLeft,
-} from 'd3';
+import { select, scaleLinear, scaleSymlog, axisBottom, axisLeft } from 'd3';
 import { margin, width, height } from './settings';
 
 fetch('./pairs_bars.json')
   .then(res => res.json())
-  .then((data: Data) => renderHistogramDate(data['2021-10-05'], offices)); //TODO: Render all dates
+  .then(init);
 
 type Data = {
   [date: string]: DataDate;
@@ -36,92 +29,152 @@ const offices: Office[] = [
   'Tampere',
 ];
 
-function renderHistogramDate(data: DataDate, offices: Office[]) {
-  const top = select('#pairs_bars');
-  top.selectChildren().remove();
+function init(data: Data) {
+  const dates = Object.keys(data);
+  const n = dates.length - 1;
 
-  const svg = top
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
+  const span = document.querySelector('#pairs_bars_value')!;
 
-  const x = scaleLinear().domain([1, data.maxX]).range([0, width]);
+  const slider = document.querySelector(
+    '#pairs_bars_date'
+  )! as HTMLInputElement;
+  slider.min = '0';
+  slider.max = `${n}`;
+  slider.value = `${n}`;
 
-  let ticksX = [1];
-  for (let i = 10; i < data.maxX - 4; i += 10) {
-    ticksX.push(i);
-  }
-  ticksX.push(data.maxX);
+  span.textContent = dates[n];
 
-  svg
-    .append('g')
-    .attr('transform', `translate(0, ${height})`)
-    .call(axisBottom(x).tickValues(ticksX));
+  const last = data[dates[n]];
+  const renderHistogram = makeRenderHistogram(data, last.maxX, last.maxY);
 
-  svg
-    .append('text')
-    .attr(
-      'transform',
-      `translate(${width / 2}, ${height + margin.bottom - 15})`
-    )
-    .style('text-anchor', 'middle')
-    .text('# of times a unique pair has met in the office');
+  slider.addEventListener('input', ev => {
+    const date = dates[parseInt((ev.target as HTMLInputElement).value)];
+    span.textContent = date;
+    renderHistogram(date, offices);
+  });
 
-  const y = scaleSymlog().domain([0, data.maxY]).range([height, 0]);
+  const button = document.querySelector(
+    '#pairs_bars_play'
+  )! as HTMLInputElement;
+  let isPlaying = undefined;
+  button.addEventListener('click', () => {
+    if (isPlaying) {
+      clearInterval(isPlaying);
+      button.textContent = 'Play';
+      isPlaying = undefined;
+    } else {
+      isPlaying = setInterval(() => {
+        const curr = parseInt(slider.value);
+        const next = (curr + 1) % dates.length;
+        if (next === n) {
+          clearInterval(isPlaying);
+          button.textContent = 'Play';
+          isPlaying = undefined;
+        }
 
-  svg.append('g').call(
-    axisLeft(y)
-      .tickValues([
-        0,
-        1,
-        2,
-        5,
-        10,
-        20,
-        50,
-        100,
-        200,
-        500,
-        1000,
-        2000,
-        5000,
-        data.maxY,
-      ])
-      .tickSizeInner(-width - 5)
-      .tickSizeOuter(3)
-  );
-
-  svg
-    .append('text')
-    .attr('transform', 'rotate(-90)')
-    .attr('y', 0 - margin.left)
-    .attr('x', 0 - height / 2)
-    .attr('dy', '1em')
-    .style('text-anchor', 'middle')
-    .text('# of unique pairs that met x times in the office');
-
-  const barWidth = width / data.maxX - 2;
-
-  let combined = {};
-
-  for (const o of offices) {
-    for (const [x, y] of Object.entries(data[o])) {
-      combined[x] = (combined[x] ?? 0) + y;
+        slider.value = `${next}`;
+        span.textContent = dates[next];
+        renderHistogram(dates[next], offices);
+      }, 250);
+      button.textContent = 'Pause';
     }
-  }
+  });
 
-  const arr = Object.entries(combined).map(([k, v]) => ({
-    x: parseInt(k),
-    y: v as number,
-  }));
+  renderHistogram(dates[n], offices);
+}
 
-  svg
-    .selectAll('.bar')
-    .data(arr)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar')
-    .attr('x', d => x(d.x))
-    .attr('y', d => y(d.y))
-    .attr('width', barWidth)
-    .attr('height', d => height - y(d.y));
+function makeRenderHistogram(
+  data: Data,
+  maxX: number,
+  maxY: number
+): (date: string, offices: Office[]) => void {
+  return function renderHistogram(date: string, offices: Office[]) {
+    const top = select('#pairs_bars');
+    top.selectChildren().remove();
+
+    const svg = top
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    const x = scaleLinear().domain([1, maxX]).range([0, width]);
+
+    let ticksX = [1];
+    for (let i = 10; i < maxX - 4; i += 10) {
+      ticksX.push(i);
+    }
+    ticksX.push(maxX);
+
+    svg
+      .append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(axisBottom(x).tickValues(ticksX));
+
+    svg
+      .append('text')
+      .attr(
+        'transform',
+        `translate(${width / 2}, ${height + margin.bottom - 15})`
+      )
+      .style('text-anchor', 'middle')
+      .text('# of times a unique pair has met in the office');
+
+    const y = scaleSymlog().domain([0, maxY]).range([height, 0]);
+
+    svg.append('g').call(
+      axisLeft(y)
+        .tickValues([
+          0,
+          1,
+          2,
+          5,
+          10,
+          20,
+          50,
+          100,
+          200,
+          500,
+          1000,
+          2000,
+          5000,
+          maxY,
+        ])
+        .tickSizeInner(-width - 5)
+        .tickSizeOuter(3)
+    );
+
+    svg
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 0 - margin.left)
+      .attr('x', 0 - height / 2)
+      .attr('dy', '1em')
+      .style('text-anchor', 'middle')
+      .text('# of unique pairs that met x times in the office');
+
+    const barWidth = width / maxX - 2;
+
+    let combined = {};
+
+    for (const o of offices) {
+      for (const [x, y] of Object.entries(data[date][o])) {
+        combined[x] = (combined[x] ?? 0) + y;
+      }
+    }
+
+    const arr = Object.entries(combined).map(([k, v]) => ({
+      x: parseInt(k),
+      y: v as number,
+    }));
+
+    svg
+      .selectAll('.bar')
+      .data(arr)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', d => x(d.x))
+      .attr('y', d => y(d.y))
+      .attr('width', barWidth)
+      .attr('height', d => height - y(d.y));
+  };
 }
